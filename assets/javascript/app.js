ae6bc4e18f757ff92ed156f9f6de94f746a62410
Shawn Highsmith <41935319+CalderMc@users.noreply.github.com>
2019-01-28 17:15:56 -0700

    Add files via upload

---
+++ assets/javascript/app.js
@@ -0,0 +1,91 @@
+
+//make a form that takes a new button name and pushes it into the original buttons array
+//reloop through originalButtons to make newButtons now that the array is bigger
+//hint:you will need to empty the buttons div or end up with more buttons than you want appended to the document
+
+$("document").ready(function() {
+
+    var topics = ["Rameses Tarheels", "Jazz Bear", "Benny the Bull", "Cosmo Cougar", "Philly Phanatic", "Sparty", "San Antonio Coyote", "Mr. Met"];
+    var numGifs = 10;
+    var currentTopic;
+    var currentOffset = 0;
+  
+    function generateButtons() {
+      for (var i = 0; i < topics.length; i++) {
+        var newButton = $("<button>");
+        newButton.attr("class", "btn btn-info");
+        newButton.text(topics[i]);
+        $("#button-container").append(newButton);
+      }
+    };
+  
+    generateButtons();
+  
+    function addMoreGifs() {
+      var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=Rt7I8O5meHI6cuUpOtEv1IcwnovntCJK&q=" + currentTopic + "&limit=" + numGifs + "&offset=" + currentOffset;
+  
+      $.ajax({
+        method: "GET",
+        url: queryURL
+      }).then(function(response) {
+        console.log(response);
+        for (var i = 0; i < numGifs; i++) {
+          var stillURL = response.data[i].images.fixed_height_still.url;
+          var animateURL = response.data[i].images.fixed_height.url;
+          var newDiv = $("<div>");
+          var newGif = $("<img>");
+          var newGifRating = $("<p class='text-center'>");
+          newGifRating.text("Rating: " + response.data[i].rating.toUpperCase());
+          newGif.attr("src", stillURL).attr("data-still", stillURL).attr("data-animate", animateURL).attr("data-state", "still");
+          newDiv.append(newGif).append(newGifRating);
+          $("#gif-container").prepend(newDiv);
+        };
+      });
+    }
+  
+    $("div#button-container").on("click", "button", function() {
+  
+      $("#gif-container").empty();
+  
+      currentTopic = $(this).text();
+  
+      currentOffset = 0;
+  
+      addMoreGifs();
+    });
+  
+    $("button#add-more").on("click", function(event) {
+      event.preventDefault();
+      if (currentTopic) {
+        currentOffset += numGifs;
+        addMoreGifs();
+      }
+    });
+  
+    $("div#gif-container").on("click", "img", function() {
+  
+      var state = $(this).attr("data-state");
+  
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      };
+      if (state === "animate") {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      };
+    });
+  
+    $("button#add-new").on("click", function(event) {
+      event.preventDefault();
+      var newValue = $("#new-value").val().trim();
+      if (newValue.length > 0 && topics.includes(newValue) === false) {
+        $("#button-container").empty();
+        topics.push($("#new-value").val().trim());
+        generateButtons();
+      };
+    });
+    
+  
+    
+  });
